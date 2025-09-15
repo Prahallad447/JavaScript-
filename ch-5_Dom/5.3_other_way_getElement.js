7d2455ea6fd539d06106325229434cdd955f6ef6
Prahallad Maiti <prahalladmaiti555@gmail.com>
2025-09-15 22:25:44 +0530

    First Commit

---
+++ ch-5_Dom/5.3_other_way_getElement.js
@@ -0,0 +1,17 @@
+//  Other way to get html element 
+
+let resultElements = document.querySelectorAll('p')
+console.log(resultElements)
+
+
+// Get element by tagname
+let tagName = document.getElementsByTagName('p')
+console.log(tagName)
+
+// Get element by class name
+let className = document.getElementsByClassName('coderdost')
+console.log(className)
+
+// Get element by id
+let idName = document.getElementById('coders')
+console.log(idName)
