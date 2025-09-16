eb699d5ab10e456fe6f66f68e28c76dae79db82e
Prahallad Maiti <prahalladmaiti555@gmail.com>
2025-09-16 08:33:36 +0530

    First commit

--- ch-5_Dom/dom-intro.js
+++
@@ -1,9 +0,0 @@
-// JavaScript DOM (Document Object Model)
-
-// Created by browser as the html load into the browser.
-
-// Browser create object of html called 'document object' .
-
-// In Document object there is model of complete html in tree like structure
-
-console.log(document)
