09003aad3f44060bd8f777453deab442ed6280d9
Prahallad Maiti <prahalladmaiti555@gmail.com>
2025-09-17 18:40:30 +0530

    First Commit

---
+++ ch-5_Dom/5.7-add-remove-replace-class.js
@@ -0,0 +1,13 @@
+// Add , remove , replace class of element
+
+let heading = document.querySelector('h2')
+
+// add class
+heading.classList.add('newClass')
+
+// remove class
+heading.classList.remove('coderdost')
+
+
+// replace class
+heading.classList.replace('newClass', 'coderdost')
