ff809515abc4ab678771815de8ed70381c0dd022
Prahallad Maiti <prahalladmaiti555@gmail.com>
2025-09-17 18:38:35 +0530

    First Commit

---
+++ ch-5_Dom/5.6-adding-style.js
@@ -0,0 +1,13 @@
+// Adding the style
+
+
+let heading = document.querySelector('h1')
+
+heading.style.color = 'blue'
+heading.style.backgroundColor = 'yellow'
+heading.style.margin = '20px'
+heading.style.textAlign = 'center'
+heading.style.padding = '10px'
+heading.style.borderRadius = '10px'
+heading.style.fontFamily = 'Arial, sans-serif'
+heading.style.boxShadow = '0 4px 8px rgba(5, 5, 5, 0.1)'
