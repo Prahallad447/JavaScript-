64fd5fec0c707be69e9a3951c2b030eb8a67c76d
Prahallad Maiti <prahalladmaiti555@gmail.com>
2025-09-25 21:45:56 +0530

    First Commit

---
+++ ch-6_DOM-Forms/6.4-keyboard-event.js
@@ -0,0 +1,36 @@
+// Keyboard event
+
+let userNamePattern = /^[A-Za-z]{6,12}$/ 
+
+let user = document.querySelector('#name')
+let form = document.querySelector('.sign-up-form')
+// let password = document.querySelector('#password')
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log(form.email.value ,form.password.value)
+
+    let result =userNamePattern.test(form.userName.value)
+
+    console.log(result == true)
+
+    if(result){
+        console.log("username is valid")
+    }
+    else{
+        console.log("Username is not valid ! Try again..!")
+    }
+})
+
+
+// Live Feedback
+
+user.addEventListener('keyup', (e) => {
+    if(userNamePattern.test(e.target.value)){
+        user.setAttribute('class' , 'success')
+    }
+    else{
+       user.setAttribute('class' , 'error')
+    }
+})
