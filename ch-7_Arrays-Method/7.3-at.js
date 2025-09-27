f128fcd9891ce6b1abad69b56a14882af38d701d
Prahallad Maiti <prahalladmaiti555@gmail.com>
2025-09-27 21:46:36 +0530

    First Commit

---
+++ ch-7_Arrays-Method/7.3-at.js
@@ -0,0 +1,22 @@
+//  Working with Array (simple methods)
+
+// at method
+
+let nums = [25,14,89,36,102]
+
+console.log(nums[0])
+console.log(nums.at(1))
+
+// Getting last element of the array
+
+console.log(nums[nums.length-1])
+
+console.log(nums.slice(-1)[0])
+
+console.log(nums.at(-1));
+
+// at method can also work on strings
+
+let name = "Larray"
+console.log(name.at(-2))
+
