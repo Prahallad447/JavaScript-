72d9489e4337d5437709e55b26cfb6418240fa5c
Prahallad Maiti <prahalladmaiti555@gmail.com>
2025-09-30 12:26:35 +0530

    First Commit

---
+++ ch-7_Arrays-Method/7.13-chaining-method.js
@@ -0,0 +1,10 @@
+// Array Method
+
+// Chain of methods
+
+let nums = [1,2,3,4,5,6]
+
+let result = nums.slice(0,3).push(89).splice(1,1,10).push(8,56);
+// Push method reeturn new length of the array
+
+console.log(result)
